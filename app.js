2bcbca11a51b23972c16f22cfeb9c1a598abc904
simonppg <simonppg@gmail.com>
2017-08-29 22:32:56 -0500

    Cambiando de graficas a unas mas simples

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
+var players = ["yo", "tu", "Yellow", "Green", "Purple", "Orange"];
+var results = [12, 19, 3, 5, 2, 3];
+var tournament = [{
+  label: players,
+  data : results,
+  backgroundColor: [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)'
+  ],
+  borderColor: [
+    'rgba(255,99,132,1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+  ],
+  borderWidth: 1
+}]
+
 var main = function() {
-  alert("Test");
+  var ctx = document.getElementById("grafica");
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: players,
+      datasets: tournament
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  });
 }
 
 $(document).ready(main);
